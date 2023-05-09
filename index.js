16f07b134ddd7184134b967d316c45f14b25947f
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-05-08 21:22:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
-// const usuarios = {
-//     nome:"Maria",
-//     idade:20,
-//     endereco:[
-//         "Rua do bobo, numero 0",
-//         "Rua qualquer, numero 120"
-// ]
-// };
+const usuarios = {
+    nome:"Maria",
+    idade:20,
+    endereco:[
+        "Rua do bobo, numero 0",
+        "Rua qualquer, numero 120"
+]
+};
 
 
-// console.log(usuarios); // = Maria, idade: 20, endereco: ...
-// // console.log(usuarios.nome); // = Maria
-// usuarios.nome = "Antonieta"
-// console.log(usuarios); // = Antonieta
+console.log(usuarios); // = Maria, idade: 20, endereco: ...
+// console.log(usuarios.nome); // = Maria
+usuarios.nome = "Antonieta"
+console.log(usuarios); // = Antonieta
 
 
-//// ex. 1 objetos
+// ex. 1 objetos
 
-// const filme = {
-// Direcao:"Tim Burton",
-// NomeDoFilme:"O estranho mundo de Jack",
-// AnoLancamento:1993,
-// Elenco:["Danny Elfman", 
-// "Chris Sarandon",
-// "Catherine O'Hara" ],
-// Visto:true
-// }
+const filme = {
+Direcao:"Tim Burton",
+NomeDoFilme:"O estranho mundo de Jack",
+AnoLancamento:1993,
+Elenco:["Danny Elfman", 
+"Chris Sarandon",
+"Catherine O'Hara" ],
+Visto:true
+}
 
-// console.log(filme.Direcao);
-// console.log(filme.NomeDoFilme);
-// console.log(filme["AnoLancamento"]);
-// console.log(filme["Elenco"]);
-// console.log(filme["Visto"]);
-
-
-// //ex. 2 objetos
-
-// const pessoa = {
-//     Nome:"Marven",
-//     Idade:17,
-//     GeneroMusicalPreferido:"Rock"
-// }
-// console.log(
-// "O nome da pessoa é" ,pessoa.Nome,
-//  "ela tem" ,pessoa.Idade, 
-//  "anos e gosta muito de" ,pessoa.GeneroMusicalPreferido);
+console.log(filme.Direcao);
+console.log(filme.NomeDoFilme);
+console.log(filme["AnoLancamento"]);
+console.log(filme["Elenco"]);
+console.log(filme["Visto"]);
 
 
-// //ex.3 objetos
+//ex. 2 objetos
+
+const pessoa = {
+    Nome:"Marven",
+    Idade:17,
+    GeneroMusicalPreferido:"Rock"
+}
+console.log(
+"O nome da pessoa é" ,pessoa.Nome,
+ "ela tem" ,pessoa.Idade, 
+ "anos e gosta muito de" ,pessoa.GeneroMusicalPreferido);
+
+
+//ex.3 objetos
 
 const endereco = {
     rua:"Rua do bobo",
