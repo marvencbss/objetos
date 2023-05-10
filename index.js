bf192b4d4ebdc268cf8dafa61492e877abc1ea7e
marvencbss <130517545+marvencbss@users.noreply.github.com>
2023-05-09 21:24:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -61,3 +61,117 @@ const pessoa = {
     enderecoPessoa:endereco
 }
 console.log(pessoa);
+
+
+///////////////////////////////////////////continuação - 09/05//////////////////////////////////////////////
+
+const endereco = {
+    rua:"Rua do bobo",
+    numero:0,
+    cidade:"São Leopoldo",
+    estado:"Rio Grande Do Sul",
+}
+endereco.cep = "047-824-150-55"
+
+const endereco2 = {
+ ...endereco,
+ cep:"21-32104"
+}
+
+const pessoa = {
+    nome:"Gabriel",
+    idade:25,
+    generoMusical: "MPB",
+    enderecoPessoa:[endereco,endereco2],
+}
+console.log(pessoa.enderecoPessoa[0].cidade);
+
+
+// ex - interpretação de código
+
+//1)
+
+const filme = {
+	nome: "Auto da Compadecida", 
+	ano: 2000, 
+	elenco: [
+		"Matheus Nachtergaele", "Selton Mello", "Denise Fraga", 
+		"Virginia Cavendish"
+		], 
+	transmissoesHoje: [
+		{canal: "Telecine", horario: "21h"}, 
+		{canal: "Canal Brasil", horario: "19h"}, 
+		{canal: "Globo", horario: "14h"}
+		]
+}
+
+console.log(filme.elenco[0])
+console.log(filme.elenco[filme.elenco.length - 1])
+console.log(filme.transmissoesHoje[2])
+
+//a) 
+//Matheus Nachtergaele
+//Virginia Cavendish
+//"Canal Globo", horario: "14h"
+
+
+//2)
+
+const cachorro = {
+	nome: "Juca", 
+	idade: 3, 
+	raca: "SRD"
+}
+
+const gato = {...cachorro, nome: "Juba"}
+
+const tartaruga = {...gato, nome: gato.nome.replaceAll("a", "o")}
+
+console.log(cachorro)
+console.log(gato)
+console.log(tartaruga)
+
+// a)
+// idade: 3
+// nome: "Juca"
+// raca: "SRD"
+
+// idade: 3
+// nome: "Juba"
+// raca: "SRD"
+
+// idade: 3
+// nome: "Jubo"
+// raca: "SRD"
+
+
+// b) copiar
+
+
+// 3)
+
+function minhaFuncao(objeto, propriedade) {
+	return objeto[propriedade]
+}
+
+const pessoa = {
+  nome: "Caio", 
+  idade: 23, 
+  backender: false
+}
+
+console.log(minhaFuncao(pessoa, "backender"))
+console.log(minhaFuncao(pessoa, "altura"))
+
+// a) false, undefined
+// b)backender é uma variável definida como false, e "altura" não consta no objeto
+
+
+// ex. 1 - escrita de código
+
+// Exemplo de entrada
+const pessoa = {
+    nome: "Vitor Hugo", 
+    apelidos: ["Vitinho", "Vitão", "Vit"]
+ }
+ console.log("Eu sou Vitor, mas pode me chamar de: Vitinho, Vitão ou Vit");
